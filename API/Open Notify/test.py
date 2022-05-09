3ae5e9e2e3e2b135f1766917457ea34c3b76d2fd
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-05-09 15:08:49 +0530

    open notify II

--- API/Open Notify/test.py
+++ API/Open Notify/test.py
@@ -2,11 +2,12 @@ import requests
 import json
 
 response = requests.get('http://api.open-notify.org/astros.json')
+res = requests.get('http://api.open-notify.org/iss-now.json')
 
-# print(response.status_code)
-
+# print(res.status_code)
+print(f"\nPosition of the ISS : ({res.json()['iss_position']['longitude']}, {res.json()['iss_position']['latitude']})")
 print(f"\nNo. of people in the ISS : {response.json()['number']}")
 
-print(f"List of people in the ISS : ")
+print("\nList of people in the ISS : ")
 for i in response.json()['people']:
     print(i['name'])
