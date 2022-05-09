11366dbc0f8330eba2640006cabb00824b95c4ab
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-05-09 22:14:41 +0530

    heart

--- Turtle/Heart/heart.py
+++ Turtle/Heart/heart.py
@@ -1,19 +1,37 @@
 from turtle import *
 
-pensize(1)
-Screen().bgcolor("yellow")
-begin_fill()
-color('black')
-# left(60)
-# circle(100, steps=3)
-# left(120)
-# circle(100, steps=3)
-# left(120)
-# circle(100, steps=3)
-# left(120)
-
-circle(50)
-end_fill()
+# Creating a turtle object(pen)
+bgcolor('black')
 hideturtle()
+
+# Defining a method to draw curve
+def curve():
+    speed(1)
+    for i in range(200):
+        right(1)
+        forward(1)
+
+# Defining method to draw a full heart
+def heart():
+    pensize(3)
+    color('yellow', 'red')
+    begin_fill()
+    left(140)
+    forward(113)
+    curve()
+    left(120)
+    curve()
+    forward(112)
+    end_fill()
+
+# Defining method to write text
+def txt():
+    penup()
+    setpos(-68, 95)
+    pendown()
+
+# Draw a heart
+heart()
+txt()
 
 done()
